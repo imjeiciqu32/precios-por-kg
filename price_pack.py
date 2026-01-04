b0818fd4c20f2e62deb3a06811664f6ad7db0399
imjeiciqu32 <chavexchema@gmail.com>
2026-01-04 14:16:07 -0600

    Update price_pack.py

--- price_pack.py
+++ price_pack.py
@@ -1,24 +1,40 @@
-# --- PLANTILLAS PARA PRICE PACK ARCHITECTURE ---
-# Formato solicitado: Primera letra Mayúscula, g minúscula para gramajes.
 
 PLANTILLAS_PP = {
-    "PP - BOTANAS DE HARINA": [
-        # MI TIERRA
-        {"Canal": "Tradicional", "Familia": "Mi tierra", "Producto": "Mi tierra 50g", "Ocasión": "Individual", "Gramaje (g)": 50, "Precio ($)": 20.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
-        {"Canal": "Autoservicio", "Familia": "Mi tierra", "Producto": "Mi tierra 50g", "Ocasión": "Individual", "Gramaje (g)": 50, "Precio ($)": 20.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
-        {"Canal": "Conveniencia", "Familia": "Mi tierra", "Producto": "Mi tierra 50g", "Ocasión": "Individual", "Gramaje (g)": 50, "Precio ($)": 26.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
+    "TAKIS": [
+        # INSTITUCIONALES (INST.)
+        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 5.0},
+        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "MIX MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 5.0},
+        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "TAKIS 56g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 56, "Precio ($)": 6.0},
+        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 19.0},
         
-        # CHIPOTLES
-        {"Canal": "Tradicional", "Familia": "Chipotles", "Producto": "Chipotles 65g", "Ocasión": "Individual", "Gramaje (g)": 65, "Precio ($)": 16.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
-        {"Canal": "Autoservicio", "Familia": "Chipotles", "Producto": "Chipotles 55g", "Ocasión": "Familiar", "Gramaje (g)": 55, "Precio ($)": 10.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
-        {"Canal": "Autoservicio", "Familia": "Chipotles", "Producto": "Chipotles 200g", "Ocasión": "Familiar", "Gramaje (g)": 200, "Precio ($)": 44.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
-        {"Canal": "Conveniencia", "Familia": "Chipotles", "Producto": "Chipotles 80g", "Ocasión": "Hambre", "Gramaje (g)": 80, "Precio ($)": 26.0, "Fabricante": "BARCEL", "SOM (%)": 0.0},
+        # MAYOREO (MAY.)
+        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 7.0},
+        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "MIX MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 7.0},
+        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 56g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 56, "Precio ($)": 9.0},
         
-        # TOSTACHOS
-        {"Canal": "Tradicional", "Familia": "Tostachos", "Producto": "Tostachos 75g", "Ocasión": "Individual", "Gramaje (g)": 75, "Precio ($)": 16.0, "Fabricante": "BARCEL", "SOM (%)": 0.0}
+        # CLUBES (CBS)
+        {"Canal": "CLUBES", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 700g", "Ocasión": "TRANSFORMADOR", "Gramaje (g)": 700, "Precio ($)": 125.0},
+        
+        # DETALLE (DT)
+        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 10.0},
+        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 70g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 70, "Precio ($)": 20.0},
+        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 45.0},
+        
+        # AUTOSERVICIO (AS)
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "MINI TAKIS FUEGO 35g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 35, "Precio ($)": 10.0},
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 94g", "Ocasión": "HAMBRE", "Gramaje (g)": 94, "Precio ($)": 25.0},
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 47.0},
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 240g", "Ocasión": "FAMILIAR", "Gramaje (g)": 240, "Precio ($)": 59.0},
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 340g", "Ocasión": "REUNIÓN", "Gramaje (g)": 340, "Precio ($)": 75.0},
+        
+        # CONVENIENCIA (CNV)
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 75g", "Ocasión": "HAMBRE", "Gramaje (g)": 75, "Precio ($)": 28.0},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS 80g", "Ocasión": "HAMBRE", "Gramaje (g)": 80, "Precio ($)": 28.0},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 55.0},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 260g", "Ocasión": "REUNIÓN", "Gramaje (g)": 260, "Precio ($)": 65.0}
     ],
     
-    "PP - EJEMPLO VACÍO": [
-        {"Canal": "Tradicional", "Familia": "Marca", "Producto": "Producto 100g", "Ocasión": "Individual", "Gramaje (g)": 100, "Precio ($)": 20.0, "Fabricante": "BARCEL", "SOM (%)": 0.0}
+    "CHIPOTLES": [
+        # Aquí irían los datos de Chipotles siguiendo la misma estructura
     ]
 }
