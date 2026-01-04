7c070637bcf4c10c27b1c00f0717f198c5c23977
imjeiciqu32 <chavexchema@gmail.com>
2026-01-04 14:25:14 -0600

    Update price_pack.py

--- price_pack.py
+++ price_pack.py
@@ -1,38 +1,37 @@
-
+# --- ARCHIVO: price_pack.py ---
 PLANTILLAS_PP = {
     "TAKIS": [
-        # INSTITUCIONALES (INST.)
-        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 5.0},
-        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "MIX MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 5.0},
-        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "TAKIS 56g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 56, "Precio ($)": 6.0},
-        {"Canal": "INSTITUCIONALES", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 19.0},
+        # INSTITUCIONALES
+        {"Canal": "INSTITUCIONALES", "Producto": "TAKIS FUEGO 200g", "Precio ($)": 19.0, "Gramaje (g)": 200},
+        {"Canal": "INSTITUCIONALES", "Producto": "TAKIS 56g", "Precio ($)": 6.0, "Gramaje (g)": 56},
+        {"Canal": "INSTITUCIONALES", "Producto": "MINI TAKIS 35g", "Precio ($)": 5.0, "Gramaje (g)": 35},
+        {"Canal": "INSTITUCIONALES", "Producto": "MIX MINI TAKIS 35g", "Precio ($)": 5.4, "Gramaje (g)": 35},
         
-        # MAYOREO (MAY.)
-        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 7.0},
-        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "MIX MINI TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 7.0},
-        {"Canal": "MAYOREO", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 56g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 56, "Precio ($)": 9.0},
+        # MAYOREO
+        {"Canal": "MAYOREO", "Producto": "TAKIS FUEGO 56g", "Precio ($)": 9.0, "Gramaje (g)": 56},
+        {"Canal": "MAYOREO", "Producto": "TAKIS FUEGO 700g", "Precio ($)": 125.0, "Gramaje (g)": 700},
+        {"Canal": "MAYOREO", "Producto": "TAKIS 200g", "Precio ($)": 45.0, "Gramaje (g)": 200},
         
-        # CLUBES (CBS)
-        {"Canal": "CLUBES", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 700g", "Ocasión": "TRANSFORMADOR", "Gramaje (g)": 700, "Precio ($)": 125.0},
+        # CLUBES
+        {"Canal": "CLUBES", "Producto": "TAKIS 35g", "Precio ($)": 10.0, "Gramaje (g)": 35},
+        {"Canal": "CLUBES", "Producto": "TAKIS 70g", "Precio ($)": 20.0, "Gramaje (g)": 70},
+        {"Canal": "CLUBES", "Producto": "TAKIS 200g", "Precio ($)": 45.0, "Gramaje (g)": 200},
         
-        # DETALLE (DT)
-        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 35g", "Ocasión": "BITES", "Gramaje (g)": 35, "Precio ($)": 10.0},
-        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 70g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 70, "Precio ($)": 20.0},
-        {"Canal": "DETALLE", "Familia": "TAKIS", "Producto": "TAKIS 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 45.0},
+        # DETALLE
+        {"Canal": "DETALLE", "Producto": "MINI TAKIS FUEGO 35g", "Precio ($)": 10.0, "Gramaje (g)": 35},
+        {"Canal": "DETALLE", "Producto": "TAKIS 94g", "Precio ($)": 25.0, "Gramaje (g)": 94},
         
-        # AUTOSERVICIO (AS)
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "MINI TAKIS FUEGO 35g", "Ocasión": "INDIVIDUAL", "Gramaje (g)": 35, "Precio ($)": 10.0},
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 94g", "Ocasión": "HAMBRE", "Gramaje (g)": 94, "Precio ($)": 25.0},
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 47.0},
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 240g", "Ocasión": "FAMILIAR", "Gramaje (g)": 240, "Precio ($)": 59.0},
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 340g", "Ocasión": "REUNIÓN", "Gramaje (g)": 340, "Precio ($)": 75.0},
+        # AUTOSERVICIO
+        {"Canal": "AUTOSERVICIO", "Producto": "TAKIS BLUE HEAT 200g", "Precio ($)": 47.0, "Gramaje (g)": 200},
+        {"Canal": "AUTOSERVICIO", "Producto": "TAKIS 240g", "Precio ($)": 59.0, "Gramaje (g)": 240},
+        {"Canal": "AUTOSERVICIO", "Producto": "TAKIS 340g", "Precio ($)": 75.0, "Gramaje (g)": 340},
         
-        # CONVENIENCIA (CNV)
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 75g", "Ocasión": "HAMBRE", "Gramaje (g)": 75, "Precio ($)": 28.0},
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS 80g", "Ocasión": "HAMBRE", "Gramaje (g)": 80, "Precio ($)": 28.0},
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200g", "Ocasión": "FAMILIAR", "Gramaje (g)": 200, "Precio ($)": 55.0},
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 260g", "Ocasión": "REUNIÓN", "Gramaje (g)": 260, "Precio ($)": 65.0}
-    ],
+        # CONVENIENCIA
+        {"Canal": "CONVENIENCIA", "Producto": "TAKIS BLUE HEAT 75g", "Precio ($)": 28.0, "Gramaje (g)": 75},
+        {"Canal": "CONVENIENCIA", "Producto": "TAKIS 80g", "Precio ($)": 28.0, "Gramaje (g)": 80},
+        {"Canal": "CONVENIENCIA", "Producto": "TAKIS FUEGO 260g", "Precio ($)": 65.0, "Gramaje (g)": 260}
+    ]
+}
     
     "CHIPOTLES": [
         # Aquí irían los datos de Chipotles siguiendo la misma estructura
