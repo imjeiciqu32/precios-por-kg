4858d2bcc2940474b6272a1e00740b45943662c7
imjeiciqu32 <chavexchema@gmail.com>
2026-01-05 10:26:03 -0600

    Update prueba3.py

--- prueba3.py
+++ prueba3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import base64  # <--- ESTA ES LA LÍNEA QUE FALTA
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import os
