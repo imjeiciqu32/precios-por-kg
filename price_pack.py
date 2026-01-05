d6f4aeab6a08bd990f859877cebe1f46c3fd9b43
imjeiciqu32 <chavexchema@gmail.com>
2026-01-05 09:45:16 -0600

    Update price_pack.py

--- price_pack.py
+++ price_pack.py
@@ -25,13 +25,239 @@ PLANTILLAS_PP = {
         {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 94G", "Precio ($)": 25.0, "Gramaje (g)": 94},
         {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 200G", "Precio ($)": 47.0, "Gramaje (g)": 200},
         {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 240G", "Precio ($)": 59.0, "Gramaje (g)": 240},
-        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 340G", "Precio ($)": 75.0, "Gramaje (g)": 340},
+        {"Canal": "AUTOSERVICIO", "Familia": "TAKIS", "Producto": "TAKIS 340G", "Precio ($)": 80.0, "Gramaje (g)": 340},
         
         # --- CONVENIENCIA (CNV) ---
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 75G", "Precio ($)": 28.0, "Gramaje (g)": 75},
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS 80G", "Precio ($)": 28.0, "Gramaje (g)": 80},
-        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200G", "Precio ($)": 55.0, "Gramaje (g)": 200},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS 56G", "Precio ($)": 22.0, "Gramaje (g)": 56}
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS BLUE HEAT 75G", "Precio ($)": 30.0, "Gramaje (g)": 75},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS 80G", "Precio ($)": 30.0, "Gramaje (g)": 80},
+        {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 200G", "Precio ($)": 58.0, "Gramaje (g)": 200},
         {"Canal": "CONVENIENCIA", "Familia": "TAKIS", "Producto": "TAKIS FUEGO 260G", "Precio ($)": 65.0, "Gramaje (g)": 260}
+    ],
+    "CHIPS": [
+        # --- INSTITUCIONALES (INST.) ---
+        {"Canal": "INSTITUCIONALES", "Familia": "CHIPS", "Producto": "CHIP'S 18G", "Precio ($)": 4.8, "Gramaje (g)": 18},
+        {"Canal": "INSTITUCIONALES", "Familia": "CHIPS", "Producto": "CHIP'S 42G", "Precio ($)": 8.7, "Gramaje (g)": 42},
+        {"Canal": "INSTITUCIONALES", "Familia": "CHIPS", "Producto": "CHIP'S 240G", "Precio ($)": 48.4, "Gramaje (g)": 240},
+        {"Canal": "INSTITUCIONALES", "Familia": "CHIPS", "Producto": "CHIP'S 500G", "Precio ($)": 92.1, "Gramaje (g)": 500},
+        
+        # --- MAYOREO (MAY.) ---
+        {"Canal": "MAYOREO", "Familia": "CHIPS", "Producto": "MINI CHIP'S 18G", "Precio ($)": 5.5, "Gramaje (g)": 18},
+        {"Canal": "MAYOREO", "Familia": "CHIPS", "Producto": "MINI CHIP'S MIX 18G", "Precio ($)": 5.6, "Gramaje (g)": 18},
+        {"Canal": "MAYOREO", "Familia": "CHIPS", "Producto": "CHIP'S 10 PACK 42G", "Precio ($)": 14.5, "Gramaje (g)": 42},
+        
+        # --- CLUBES (CBS) ---
+        {"Canal": "CLUBES", "Familia": "CHIPS", "Producto": "CHIP'S 4 PACK 170G", "Precio ($)": 52.0, "Gramaje (g)": 170},
+        {"Canal": "CLUBES", "Familia": "CHIPS", "Producto": "CHIP'S SAL 500G", "Precio ($)": 138.0, "Gramaje (g)": 500},
+        {"Canal": "CLUBES", "Familia": "CHIPS", "Producto": "CHIP'S JALAPEÑO 655G", "Precio ($)": 165.0, "Gramaje (g)": 655},
+        {"Canal": "CLUBES", "Familia": "CHIPS", "Producto": "CHIP'S SAL 800G", "Precio ($)": 195.0, "Gramaje (g)": 800},
+        
+        # --- DETALLE (DT) ---
+        {"Canal": "DETALLE", "Familia": "CHIPS", "Producto": "CHIP'S 42G", "Precio ($)": 15.0, "Gramaje (g)": 42},
+        {"Canal": "DETALLE", "Familia": "CHIPS", "Producto": "CHIP'S 52G", "Precio ($)": 20.0, "Gramaje (g)": 52},
+        {"Canal": "DETALLE", "Familia": "CHIPS", "Producto": "CHIP'S 170G", "Precio ($)": 55.0, "Gramaje (g)": 170},
+        
+        # --- AUTOSERVICIO (AS) ---
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "MINI CHIP'S (E. BA, BAE Y MB) 18G", "Precio ($)": 10.0, "Gramaje (g)": 18},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S 42G", "Precio ($)": 15.0, "Gramaje (g)": 42},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S 55G", "Precio ($)": 25.0, "Gramaje (g)": 55},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S 100G", "Precio ($)": 40.0, "Gramaje (g)": 100},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S 150G", "Precio ($)": 59.0, "Gramaje (g)": 150},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S (EXCLUSIVO WALMART) 160G", "Precio ($)": 59.0, "Gramaje (g)": 160},
+        {"Canal": "AUTOSERVICIO", "Familia": "CHIPS", "Producto": "CHIP'S 240G", "Precio ($)": 85.0, "Gramaje (g)": 240},
+        
+        # --- CONVENIENCIA (CNV) ---
+        {"Canal": "CONVENIENCIA", "Familia": "CHIPS", "Producto": "CHIP'S (CADENAS REGIONALES) 42G", "Precio ($)": 24.0, "Gramaje (g)": 42},
+        {"Canal": "CONVENIENCIA", "Familia": "CHIPS", "Producto": "CHIP'S 60G", "Precio ($)": 32.0, "Gramaje (g)": 60},
+        {"Canal": "CONVENIENCIA", "Familia": "CHIPS", "Producto": "CHIP'S 100G", "Precio ($)": 49.0, "Gramaje (g)": 100},
+        {"Canal": "CONVENIENCIA", "Familia": "CHIPS", "Producto": "CHIP'S 170G", "Precio ($)": 65.0, "Gramaje (g)": 170}
+    ],
+    "BIG MIX": [
+        # --- DETALLE (DT) ---
+        {"Canal": "DETALLE", "Familia": "Big Mix", "Producto": "Big Mix 38g", "Precio ($)": 10.0, "Gramaje (g)": 38},
+        {"Canal": "DETALLE", "Familia": "Big Mix", "Producto": "Big Mix 80g", "Precio ($)": 20.0, "Gramaje (g)": 80},
+        {"Canal": "DETALLE", "Familia": "Big Mix", "Producto": "Big Mix 185g", "Precio ($)": 40.0, "Gramaje (g)": 185},
+        
+        # --- AUTOSERVICIO (AS) ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Big Mix", "Producto": "Big Mix 200g", "Precio ($)": 52.0, "Gramaje (g)": 200},
+        
+        # --- CONVENIENCIA (CNV) ---
+        {"Canal": "CONVENIENCIA", "Familia": "Big Mix", "Producto": "Big Mix 75g", "Precio ($)": 25.0, "Gramaje (g)": 75, "Ocasión": "Hambre"},
+        {"Canal": "CONVENIENCIA", "Familia": "Big Mix", "Producto": "Big Mix Salsa Inglesa 170g", "Precio ($)": 50.0, "Gramaje (g)": 170},
+        {"Canal": "CONVENIENCIA", "Familia": "Big Mix", "Producto": "Big Mix Fuego y Queso 185g", "Precio ($)": 50.0, "Gramaje (g)": 185}
+    ],
+    "CHIPOTLES": [
+        # --- DETALLE (DT) ---
+        {"Canal": "DETALLE", "Familia": "Chipotles", "Producto": "Chipotles 65g", "Precio ($)": 18.0, "Gramaje (g)": 65},
+        
+        # --- AUTOSERVICIO (AS) ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Chipotles", "Producto": "Chipotles 55g", "Precio ($)": 10.0, "Gramaje (g)": 55},
+        {"Canal": "AUTOSERVICIO", "Familia": "Chipotles", "Producto": "Chipotles 200g", "Precio ($)": 45.0, "Gramaje (g)": 200},
+        
+        # --- CONVENIENCIA (CNV) ---
+        {"Canal": "CONVENIENCIA", "Familia": "Chipotles", "Producto": "Chipotles 80g", "Precio ($)": 28.0, "Gramaje (g)": 80}
+    ],
+    "RUNNERS": [
+        # --- INSTITUCIONALES (INST.) ---
+        {"Canal": "INSTITUCIONALES", "Familia": "Runners", "Producto": "Runners 58g", "Precio ($)": 6.5, "Gramaje (g)": 58},
+        
+        # --- MAYOREO (MAY.) ---
+        {"Canal": "MAYOREO", "Familia": "Runners", "Producto": "Mini Runners 35g", "Precio ($)": 6.6, "Gramaje (g)": 35},
+        {"Canal": "MAYOREO", "Familia": "Runners", "Producto": "Runners 58g", "Precio ($)": 8.0, "Gramaje (g)": 58},
+        
+        # --- DETALLE (DT) ---
+        {"Canal": "DETALLE", "Familia": "Runners", "Producto": "Runners 72g", "Precio ($)": 18.0, "Gramaje (g)": 72},
+        {"Canal": "DETALLE", "Familia": "Runners", "Producto": "Runners 200g", "Precio ($)": 40.0, "Gramaje (g)": 200},
+        
+        # --- AUTOSERVICIO (AS) ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Runners", "Producto": "Runners 280g", "Precio ($)": 59.0, "Gramaje (g)": 280},
+        
+        # --- CONVENIENCIA (CNV) ---
+        {"Canal": "CONVENIENCIA", "Familia": "Runners", "Producto": "Runners 80g", "Precio ($)": 28.0, "Gramaje (g)": 80}
+        {"Canal": "CONVENIENCIA", "Familia": "Runners", "Producto": "Runners 80g", "Precio ($)": 48.0, "Gramaje (g)": 200}
+    ],
+    "GOLDEN NUTS": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Golden Nuts", "Producto": "Golden Nuts 78g", "Precio ($)": 20.0, "Gramaje (g)": 78},
+        {"Canal": "DETALLE", "Familia": "Golden Nuts", "Producto": "Golden Nuts 110g", "Precio ($)": 25.0, "Gramaje (g)": 110},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Golden Nuts", "Producto": "Golden Nuts 100g", "Precio ($)": 25.0, "Gramaje (g)": 100},
+        {"Canal": "AUTOSERVICIOS", "Familia": "Golden Nuts", "Producto": "Golden Nuts 200g", "Precio ($)": 40.0, "Gramaje (g)": 200},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Golden Nuts", "Producto": "Golden Nuts 78g", "Precio ($)": 22.0, "Gramaje (g)": 78},
+        {"Canal": "CONVENIENCIA", "Familia": "Golden Nuts", "Producto": "Golden Nuts 100g", "Precio ($)": 27.0, "Gramaje (g)": 100},
+        {"Canal": "CONVENIENCIA", "Familia": "Golden Nuts", "Producto": "Golden Nuts 150g", "Precio ($)": 40.0, "Gramaje (g)": 150}
+    ],
+    "HOT NUTS": [
+        # --- INSTITUCIONALES ---
+        {"Canal": "INSTITUCIONALES", "Familia": "Hot Nuts", "Producto": "Hot Nuts 50g", "Precio ($)": 8.4, "Gramaje (g)": 50},
+        {"Canal": "INSTITUCIONALES", "Familia": "Hot Nuts", "Producto": "Hot Nuts 100g", "Precio ($)": 10.6, "Gramaje (g)": 100},
+        
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Hot Nuts", "Producto": "Hot Nuts 75g", "Precio ($)": 20.0, "Gramaje (g)": 75},
+        {"Canal": "DETALLE", "Familia": "Hot Nuts", "Producto": "Hot Nuts 115g", "Precio ($)": 25.0, "Gramaje (g)": 115},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Hot Nuts", "Producto": "Hot Nuts 100g", "Precio ($)": 27.0, "Gramaje (g)": 100},
+        {"Canal": "AUTOSERVICIOS", "Familia": "Hot Nuts", "Producto": "Hot Nuts 200g", "Precio ($)": 42.0, "Gramaje (g)": 200},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Hot Nuts", "Producto": "Hot Nuts 50g", "Precio ($)": 22.0, "Gramaje (g)": 50},
+        {"Canal": "CONVENIENCIA", "Familia": "Hot Nuts", "Producto": "Hot Nuts 100g", "Precio ($)": 35.0, "Gramaje (g)": 100},
+        {"Canal": "CONVENIENCIA", "Familia": "Hot Nuts", "Producto": "Hot Nuts 160g", "Precio ($)": 47.0, "Gramaje (g)": 160}
+    ],
+    
+    "KIYAKIS": [
+        # --- INSTITUCIONALES ---
+        {"Canal": "INSTITUCIONALES", "Familia": "Kiyakis", "Producto": "Kiyakis 95g", "Precio ($)": 11.3, "Gramaje (g)": 95},
+        
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Kiyakis", "Producto": "Kiyakis 60g", "Precio ($)": 15.0, "Gramaje (g)": 60},
+        {"Canal": "DETALLE", "Familia": "Kiyakis", "Producto": "Kiyakis 120g", "Precio ($)": 20.0, "Gramaje (g)": 120},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Kiyakis", "Producto": "Kiyakis 150g", "Precio ($)": 25.0, "Gramaje (g)": 150},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Kiyakis", "Producto": "Kiyakis 50g", "Precio ($)": 15.0, "Gramaje (g)": 50},
+        {"Canal": "CONVENIENCIA", "Familia": "Kiyakis", "Producto": "Kiyakis 95g", "Precio ($)": 22.0, "Gramaje (g)": 95}
+    ],
+    
+    "POP KARAMELADAS": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 45g", "Precio ($)": 10.0, "Gramaje (g)": 45},
+        {"Canal": "DETALLE", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 110g", "Precio ($)": 16.0, "Gramaje (g)": 110},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 90g", "Precio ($)": 17.0, "Gramaje (g)": 90},
+        {"Canal": "AUTOSERVICIOS", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 120g", "Precio ($)": 30.0, "Gramaje (g)": 120},
+        {"Canal": "AUTOSERVICIOS", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 240g", "Precio ($)": 59.0, "Gramaje (g)": 240},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 120g", "Precio ($)": 32.0, "Gramaje (g)": 120},
+        {"Canal": "CONVENIENCIA", "Familia": "Pop Karameladas", "Producto": "Pop Karameladas 260g", "Precio ($)": 58.0, "Gramaje (g)": 260}
+    ],
+    
+    "POP QUESO": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Pop Queso", "Producto": "Pop Queso 45g", "Precio ($)": 15.0, "Gramaje (g)": 45},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Pop Queso", "Producto": "Pop Queso 23g", "Precio ($)": 10.0, "Gramaje (g)": 23},
+        {"Canal": "AUTOSERVICIOS", "Familia": "Pop Queso", "Producto": "Pop Queso 110g", "Precio ($)": 42.0, "Gramaje (g)": 110},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Pop Queso", "Producto": "Pop Queso 45g", "Precio ($)": 22.0, "Gramaje (g)": 45}
+    ],
+    "VALENTONES": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Valentones", "Producto": "Valentones 60g", "Precio ($)": 16.0, "Gramaje (g)": 60},
+        {"Canal": "DETALLE", "Familia": "Valentones", "Producto": "Valentones 175g", "Precio ($)": 40.0, "Gramaje (g)": 175},
+        
+        # --- AUTOSERVICIO ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Valentones", "Producto": "Valentones 175g", "Precio ($)": 36.0, "Gramaje (g)": 175},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Valentones", "Producto": "Valentones 175g", "Precio ($)": 44.0, "Gramaje (g)": 175}
+    ],
+    "TOSTACHOS": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Tostachos", "Producto": "Tostachos 75g", "Precio ($)": 18.0, "Gramaje (g)": 75},
+        
+        # --- AUTOSERVICIO ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Tostachos", "Producto": "Tostachos 50g", "Precio ($)": 10.0, "Gramaje (g)": 50},
+        {"Canal": "AUTOSERVICIO", "Familia": "Tostachos", "Producto": "Tostachos 240g", "Precio ($)": 53.0, "Gramaje (g)": 240},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Tostachos", "Producto": "Tostachos 80g", "Precio ($)": 23.0, "Gramaje (g)": 80},
+        {"Canal": "CONVENIENCIA", "Familia": "Tostachos", "Producto": "Tostachos 200g", "Precio ($)": 53.0, "Gramaje (g)": 200}
+    ],
+    "PAPA BARCEL": [
+        # --- INSTITUCIONALES ---
+        {"Canal": "INSTITUCIONALES", "Familia": "Papa Barcel", "Producto": "Papa Barcel Sal y Adobo 38g", "Precio ($)": 9.7, "Gramaje (g)": 38},
+        {"Canal": "INSTITUCIONALES", "Familia": "Papa Barcel", "Producto": "Papa Barcel Sal 42g", "Precio ($)": 6.8, "Gramaje (g)": 42},
+        {"Canal": "INSTITUCIONALES", "Familia": "Papa Barcel", "Producto": "Papa Barcel 170g", "Precio ($)": 27.4, "Gramaje (g)": 170},
+        
+        # --- MAYOREO ---
+        {"Canal": "MAYOREO", "Familia": "Papa Barcel", "Producto": "Papa Barcel 38g", "Precio ($)": 11.0, "Gramaje (g)": 38},
+        
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Papa Barcel", "Producto": "Papa Barcel 28g", "Precio ($)": 10.0, "Gramaje (g)": 28},
+        {"Canal": "DETALLE", "Familia": "Papa Barcel", "Producto": "Papa Barcel 45g", "Precio ($)": 18.0, "Gramaje (g)": 45},
+        {"Canal": "DETALLE", "Familia": "Papa Barcel", "Producto": "Papa Barcel 170g", "Precio ($)": 55.0, "Gramaje (g)": 170},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Papa Barcel", "Producto": "Papa Barcel 170g", "Precio ($)": 55.0, "Gramaje (g)": 170},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Papa Barcel", "Producto": "Papa Barcel 90g", "Precio ($)": 39.0, "Gramaje (g)": 90},
+        {"Canal": "CONVENIENCIA", "Familia": "Papa Barcel", "Producto": "Papa Barcel 170g", "Precio ($)": 60.0, "Gramaje (g)": 170}
+    ],
+
+    "PAPATINAS": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Papatinas", "Producto": "Papatinas 31g", "Precio ($)": 16.0, "Gramaje (g)": 31},
+        
+        # --- AUTOSERVICIOS ---
+        {"Canal": "AUTOSERVICIOS", "Familia": "Papatinas", "Producto": "Papatinas 200g", "Precio ($)": 55.0, "Gramaje (g)": 200},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Papatinas", "Producto": "Papatinas 90g", "Precio ($)": 42.0, "Gramaje (g)": 90},
+        {"Canal": "CONVENIENCIA", "Familia": "Papatinas", "Producto": "Papatinas 200g", "Precio ($)": 54.0, "Gramaje (g)": 200}
+    ],
+    "CHICHARRÓN DE CERDO": [
+        # --- DETALLE ---
+        {"Canal": "DETALLE", "Familia": "Chicharrón de Cerdo", "Producto": "Chicharrón de Cerdo 30g", "Precio ($)": 18.0, "Gramaje (g)": 30},
+        {"Canal": "DETALLE", "Familia": "Chicharrón de Cerdo", "Producto": "Chicharrón de Cerdo 70g", "Precio ($)": 40.0, "Gramaje (g)": 70},
+        
+        # --- AUTOSERVICIO ---
+        {"Canal": "AUTOSERVICIO", "Familia": "Chicharrón de Cerdo", "Producto": "Chicharrón de Cerdo 90g", "Precio ($)": 50.0, "Gramaje (g)": 90},
+        
+        # --- CONVENIENCIA ---
+        {"Canal": "CONVENIENCIA", "Familia": "Chicharrón de Cerdo", "Producto": "Chicharrón de Cerdo 110g", "Precio ($)": 65.0, "Gramaje (g)": 110}
     ]
+    
 }
     
