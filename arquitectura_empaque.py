00ae3ec0018314b46072510f6df6c65ea4085a4b
imjeiciqu32 <chavexchema@gmail.com>
2026-01-12 10:33:36 -0600

    Update arquitectura_empaque.py

--- arquitectura_empaque.py
+++ arquitectura_empaque.py
@@ -1,10 +1,7 @@
 render_arquitectura_empaque = [
-    {"Producto": "Takis Fuego 240g", "Fabricante": "BARCEL", "Ancho (cm)": 18, "Alto (cm)": 28, "Gramaje (g)": 240},
-    {"Producto": "Doritos Nacho 280g", "Fabricante": "SABRITAS", "Ancho (cm)": 20, "Alto (cm)": 30, "Gramaje (g)": 280},
-    {"Producto": "Ruffles Original 240g", "Fabricante": "SABRITAS", "Ancho (cm)": 19, "Alto (cm)": 29, "Gramaje (g)": 240},
-    {"Producto": "Takis Fuego 62g", "Fabricante": "BARCEL", "Ancho (cm)": 10, "Alto (cm)": 18, "Gramaje (g)": 62},
-    {"Producto": "Doritos Nacho 58g", "Fabricante": "SABRITAS", "Ancho (cm)": 9, "Alto (cm)": 17, "Gramaje (g)": 58},
-    {"Producto": "Cheetos Flamin 57g", "Fabricante": "SABRITAS", "Ancho (cm)": 9.5, "Alto (cm)": 17.5, "Gramaje (g)": 57},
-    {"Producto": "Takis Fuego 420g", "Fabricante": "BARCEL", "Ancho (cm)": 24, "Alto (cm)": 35, "Gramaje (g)": 420},
-    {"Producto": "Doritos Nacho 370g", "Fabricante": "SABRITAS", "Ancho (cm)": 22, "Alto (cm)": 33, "Gramaje (g)": 370}
+    {"Producto": "Takis Fuego 240g", "Fabricante": "BARCEL", "Marca": "TAKIS", "Canal": "AUTOSERVICIO", "Ancho (cm)": 18, "Alto (cm)": 28},
+    {"Producto": "Doritos Nacho 280g", "Fabricante": "SABRITAS", "Marca": "DORITOS", "Canal": "AUTOSERVICIO", "Ancho (cm)": 20, "Alto (cm)": 30},
+    {"Producto": "Ruffles Original 240g", "Fabricante": "SABRITAS", "Marca": "RUFFLES", "Canal": "CONVENIENCIA", "Ancho (cm)": 19, "Alto (cm)": 29},
+    {"Producto": "Takis Fuego 62g", "Fabricante": "BARCEL", "Marca": "TAKIS", "Canal": "TRADICIONAL", "Ancho (cm)": 10, "Alto (cm)": 18},
+    {"Producto": "Doritos Nacho 58g", "Fabricante": "SABRITAS", "Marca": "DORITOS", "Canal": "TRADICIONAL", "Ancho (cm)": 9, "Alto (cm)": 17}
 ]
