369fc3d10aa5b1aeafdde11b6eaf38e99b6a5016
imjeiciqu32 <chavexchema@gmail.com>
2026-01-12 09:49:48 -0600

    Create arquitectura_empaque.py

---
+++ arquitectura_empaque.py
@@ -0,0 +1,29 @@
+data_ejemplo = [
+    # --- CANAL DETALLE (Empaques Pequeños) ---
+    {
+        "Canal": "DETALLE", 
+        "Marca": "SABRITAS", 
+        "Nombre_producto": "Cheetos Colmillo 50g", 
+        "Alto": 22.0, "Ancho": 11.0
+    },
+    {
+        "Canal": "DETALLE", 
+        "Marca": "BARCEL", 
+        "Nombre_producto": "Takis Fuego 56g", 
+        "Alto": 21.5, "Ancho": 12.0
+    },
+    
+    # --- CANAL AUTOSERVICIO (Empaques Grandes) ---
+    {
+        "Canal": "AUTOSERVICIO", 
+        "Marca": "SABRITAS", 
+        "Nombre_producto": "Doritos Nacho 145g", 
+        "Alto": 28.5, "Ancho": 16.0
+    },
+    {
+        "Canal": "AUTOSERVICIO", 
+        "Marca": "BARCEL", 
+        "Nombre_producto": "Chips Sal 160g", 
+        "Alto": 29.5, "Ancho": 17.5
+    }
+]
