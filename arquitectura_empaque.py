1fac96b6a46a38e51cf8c5090405423a4a50e25d
imjeiciqu32 <chavexchema@gmail.com>
2026-01-12 10:18:53 -0600

    Update arquitectura_empaque.py

--- arquitectura_empaque.py
+++ arquitectura_empaque.py
@@ -1,17 +1,10 @@
-render_arquitectura_empaque = {
-    "Ocasión Compartir (Fiesta)": [
-        {"Producto": "Takis Fuego 240g", "Ancho": 18, "Alto": 28, "Gramaje": 240, "Fabricante": "BARCEL"},
-        {"Producto": "Doritos Nacho 280g", "Ancho": 20, "Alto": 30, "Gramaje": 280, "Fabricante": "SABRITAS"},
-        {"Producto": "Ruffles Original 240g", "Ancho": 19, "Alto": 29, "Gramaje": 240, "Fabricante": "SABRITAS"},
-    ],
-    "Consumo Individual": [
-        {"Producto": "Takis Fuego 62g", "Ancho": 10, "Alto": 18, "Gramaje": 62, "Fabricante": "BARCEL"},
-        {"Producto": "Doritos Nacho 58g", "Ancho": 9, "Alto": 17, "Gramaje": 58, "Fabricante": "SABRITAS"},
-        {"Producto": "Cheetos Flamin 57g", "Ancho": 9.5, "Alto": 17.5, "Gramaje": 57, "Fabricante": "SABRITAS"},
-    ],
-    "Formato Familiar": [
-        {"Producto": "Takis Fuego 420g", "Ancho": 24, "Alto": 35, "Gramaje": 420, "Fabricante": "BARCEL"},
-        {"Producto": "Doritos Nacho 370g", "Ancho": 22, "Alto": 33, "Gramaje": 370, "Fabricante": "SABRITAS"},
-    ]
-}
-
+render_arquitectura_empaque = [
+    {"Producto": "Takis Fuego 240g", "Fabricante": "BARCEL", "Ancho (cm)": 18, "Alto (cm)": 28, "Gramaje (g)": 240},
+    {"Producto": "Doritos Nacho 280g", "Fabricante": "SABRITAS", "Ancho (cm)": 20, "Alto (cm)": 30, "Gramaje (g)": 280},
+    {"Producto": "Ruffles Original 240g", "Fabricante": "SABRITAS", "Ancho (cm)": 19, "Alto (cm)": 29, "Gramaje (g)": 240},
+    {"Producto": "Takis Fuego 62g", "Fabricante": "BARCEL", "Ancho (cm)": 10, "Alto (cm)": 18, "Gramaje (g)": 62},
+    {"Producto": "Doritos Nacho 58g", "Fabricante": "SABRITAS", "Ancho (cm)": 9, "Alto (cm)": 17, "Gramaje (g)": 58},
+    {"Producto": "Cheetos Flamin 57g", "Fabricante": "SABRITAS", "Ancho (cm)": 9.5, "Alto (cm)": 17.5, "Gramaje (g)": 57},
+    {"Producto": "Takis Fuego 420g", "Fabricante": "BARCEL", "Ancho (cm)": 24, "Alto (cm)": 35, "Gramaje (g)": 420},
+    {"Producto": "Doritos Nacho 370g", "Fabricante": "SABRITAS", "Ancho (cm)": 22, "Alto (cm)": 33, "Gramaje (g)": 370}
+]
