74764a53405358e2b5f7636ea7e1c7a023321c72
imjeiciqu32 <chavexchema@gmail.com>
2026-02-24 09:05:11 -0600

    Update plantillas.py

--- plantillas.py
+++ plantillas.py
@@ -643,10 +643,10 @@ PLANTILLAS = {
         # TRANSFORMADOR
         {"Producto": "SURTIDO DE SABRITAS MINIS PACK 50P (SAMS) ", "Fabricante": "SABRITAS", "Ocasión": "TRANSFORMADOR", "Precio ($)": 273.13, "Gramaje (g)": 963, "SOM (%)": 0.0},
         {"Producto": "SURTIDO DE SABRITAS MINIS PACK 50P (COSTCO) ", "Fabricante": "SABRITAS", "Ocasión": "TRANSFORMADOR", "Precio ($)": 359.0, "Gramaje (g)": 963, "SOM (%)": 0.0},      
-        {"Producto": "SURTIDO DE SABRITAS SABRINEGOCIO 43P" (SAMS), "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 479.77, "Gramaje (g)": 1788, "SOM (%)": 0.0},
-        {"Producto": "DORITOS NACHO 25P - 54G" (SAMS), "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 475.69, "Gramaje (g)": 1350, "SOM (%)": 0.0},
+        {"Producto": "SURTIDO DE SABRITAS SABRINEGOCIO 43P (SAMS)", "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 479.77, "Gramaje (g)": 1788, "SOM (%)": 0.0},
+        {"Producto": "DORITOS NACHO 25P - 54G (SAMS)", "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 475.69, "Gramaje (g)": 1350, "SOM (%)": 0.0},
         {"Producto": "SURTIDO DE SABRITAS 30P (COSTCO) ", "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 419.0, "Gramaje (g)": 1314, "SOM (%)": 0.0},
-        {"Producto": "MIX BARCEL 27P  (SAMS) ", "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 295.0, "Gramaje (g)": 1362, "SOM (%)": 0.0},
+        {"Producto": "MIX BARCEL 27P  (SAMS)", "Fabricante": "SABRITAS", "Ocasión": "B2B", "Precio ($)": 295.0, "Gramaje (g)": 1362, "SOM (%)": 0.0},
 
     ],
    
